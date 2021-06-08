60f9290107e32a825ce1774b618665b3ccddfa20
Твоя Булочка <85406297+Romb1k@users.noreply.github.com>
2021-06-08 04:43:15 +0300

    Add files via upload

---
+++ Complementing a Strand of DNA.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+	string str; cin >> str;
+	char s;
+	for (int i = 0; i < str.length(); i++)
+	{
+		s = str.at(i);
+		if (s == 'A') str[i] = 'T';
+		if (s == 'G') str[i] = 'C';
+		if (s == 'C') str[i] = 'G';
+		if (s == 'T') str[i] = 'A';
+	}
+	cout << "\n";
+	reverse(str.begin(), str.end());
+	cout << str;
+}
